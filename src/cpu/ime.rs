c03bad7fda647ccc17988e13532ce421aaac42d4
bmoxb <maxoblack@yahoo.com>
2023-02-06 10:54:24 +0000

    fix IME and add tests

--- src/cpu/ime.rs
+++ src/cpu/ime.rs
@@ -1,8 +1,8 @@
 #[derive(Default)]
 pub struct InterruptMasterEnable {
     value: bool,
-    enable_in_cycles: u8,
-    disable_in_cycles: u8,
+    enable_in_cycles: Option<u8>,
+    disable_in_cycles: Option<u8>,
 }
 
 impl InterruptMasterEnable {
@@ -11,26 +11,67 @@ impl InterruptMasterEnable {
     }
 
     pub fn cycle(&mut self) {
-        if self.enable_in_cycles == 0 {
+        if matches!(self.enable_in_cycles, Some(0)) {
             self.value = true;
+            self.enable_in_cycles.take();
             log::trace!("interrupts enabled (IME=1)");
         } else {
-            self.enable_in_cycles -= 1;
+            self.enable_in_cycles = self.enable_in_cycles.map(|x| x - 1);
         }
 
-        if self.disable_in_cycles == 0 {
+        if matches!(self.disable_in_cycles, Some(0)) {
             self.value = false;
+            self.disable_in_cycles.take();
             log::trace!("interrupts disabled (IME=0)");
         } else {
-            self.disable_in_cycles -= 1;
+            self.disable_in_cycles = self.disable_in_cycles.map(|x| x - 1);
         }
     }
 
     pub fn enable(&mut self, after_cycles: u8) {
-        self.enable_in_cycles = after_cycles;
+        self.enable_in_cycles = Some(after_cycles);
     }
 
     pub fn disable(&mut self, after_cycles: u8) {
-        self.disable_in_cycles = after_cycles;
+        self.disable_in_cycles = Some(after_cycles);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::InterruptMasterEnable;
+
+    #[test]
+    fn enable_disable_immediately() {
+        let mut ime = InterruptMasterEnable::default();
+
+        ime.enable(0);
+        assert!(!ime.enabled());
+        ime.cycle();
+        assert!(ime.enabled());
+
+        ime.disable(0);
+        assert!(ime.enabled());
+        ime.cycle();
+        assert!(!ime.enabled());
+    }
+
+    #[test]
+    fn enable_display_after_cycles() {
+        let mut ime = InterruptMasterEnable::default();
+
+        ime.enable(1);
+        for _ in 0..2 {
+            assert!(!ime.enabled());
+            ime.cycle();
+        }
+        assert!(ime.enabled());
+
+        ime.disable(3);
+        for _ in 0..4 {
+            assert!(ime.enabled());
+            ime.cycle();
+        }
+        assert!(!ime.enabled());
     }
 }
