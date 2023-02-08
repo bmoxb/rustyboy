c103dc0fd50f302d050b4274efcfcfe33d382283
bmoxb <maxoblack@yahoo.com>
2023-02-08 12:30:42 +0000

    bit get/set/reset and swap functions with tests

--- src/cpu/alu.rs
+++ src/cpu/alu.rs
@@ -158,3 +158,98 @@ pub fn rotate_right_through_carry_flag(flags: &mut Flags, x: u8) -> u8 {
 }
 
 // TODO: Tidy up flag setting?
+
+pub fn test_bit(flags: &mut Flags, bit: u8, value: u8) {
+    let mask = 1 << bit;
+    let bit_set = (value & mask) != 0;
+
+    flags
+        .set(Flag::Zero, !bit_set)
+        .set(Flag::Subtraction, false)
+        .set(Flag::HalfCarry, false);
+}
+
+pub fn set_bit(bit: u8, value: u8) -> u8 {
+    let mask = 1 << bit;
+    value | mask
+}
+
+pub fn reset_bit(bit: u8, value: u8) -> u8 {
+    let mask = !(1 << bit);
+    value & mask
+}
+
+pub fn swap_nibbles(flags: &mut Flags, value: u8) -> u8 {
+    flags
+        .set(Flag::Zero, value == 0)
+        .set(Flag::Subtraction, false)
+        .set(Flag::HalfCarry, false)
+        .set(Flag::Carry, false);
+
+    let upper = value >> 4;
+    let lower = value & 0b1111;
+    (lower << 4) + upper
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bit_testing() {
+        let tests = vec![
+            (0, 0, true),
+            (0, 0xFF, false),
+            (3, 0b1110111, true),
+            (7, 0b10001000, false),
+        ];
+
+        let mut flags = Flags::default();
+
+        for (bit, value, expected) in tests {
+            test_bit(&mut flags, bit, value);
+
+            assert_eq!(
+                expected,
+                flags.get(Flag::Zero),
+                "bit {bit} of value {value} tested, expected {expected} zero flag"
+            );
+            assert!(!flags.get(Flag::Subtraction));
+            assert!(!flags.get(Flag::HalfCarry));
+        }
+    }
+
+    #[test]
+    fn bit_setting() {
+        assert_eq!(set_bit(0, 0), 1);
+        assert_eq!(set_bit(0, 1), 1);
+        assert_eq!(set_bit(2, 0b10011), 0b10111);
+        assert_eq!(set_bit(7, 0b01111111), 0xFF);
+    }
+
+    #[test]
+    fn bit_resetting() {
+        assert_eq!(reset_bit(0, 0), 0);
+        assert_eq!(reset_bit(0, 1), 0);
+        assert_eq!(reset_bit(3, 0b1010), 0b10);
+        assert_eq!(reset_bit(7, 0b10000000), 0);
+    }
+
+    #[test]
+    fn nibble_swapping() {
+        let mut flags = Flags::default();
+        flags
+            .set(Flag::Subtraction, true)
+            .set(Flag::HalfCarry, true)
+            .set(Flag::Carry, true);
+
+        assert_eq!(swap_nibbles(&mut flags, 0xAB), 0xBA);
+        assert!(!flags.get(Flag::Zero));
+        assert!(!flags.get(Flag::Subtraction));
+        assert!(!flags.get(Flag::HalfCarry));
+        assert!(!flags.get(Flag::Carry));
+
+        assert_eq!(swap_nibbles(&mut flags, 0), 0);
+        assert!(flags.get(Flag::Zero));
+    }
+}
