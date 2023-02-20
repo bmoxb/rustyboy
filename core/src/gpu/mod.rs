ea0cd39814985b0cd73d941c72514d851a858269
bmoxb <maxoblack@yahoo.com>
2023-02-20 14:51:39 +0000

    add GPU registers

--- core/src/gpu/mod.rs
+++ core/src/gpu/mod.rs
@@ -2,12 +2,34 @@ const VRAM_SIZE: usize = 0x2000;
 
 pub struct Gpu {
     pub vram: [u8; VRAM_SIZE],
+    pub lcd_control: u8,
+    pub lcd_status: u8,
+    pub viewport_y: u8,
+    pub viewport_x: u8,
+    pub lcd_y: u8,
+    pub ly_compare: u8,
+    pub bg_palette_data: u8,
+    pub obj_palette_0_data: u8,
+    pub obj_palette_1_data: u8,
+    pub window_y: u8,
+    pub window_x: u8,
 }
 
 impl Gpu {
     pub fn new() -> Self {
         Gpu {
             vram: [0; VRAM_SIZE],
+            lcd_control: 0,
+            lcd_status: 0,
+            viewport_y: 0,
+            viewport_x: 0,
+            lcd_y: 0,
+            ly_compare: 0,
+            bg_palette_data: 0,
+            obj_palette_0_data: 0,
+            obj_palette_1_data: 0,
+            window_y: 0,
+            window_x: 0,
         }
     }
 }
