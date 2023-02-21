db607d03e6de40056e3cce1e4f8035635a0d7ae1
bmoxb <maxoblack@yahoo.com>
2023-02-21 15:32:59 +0000

    add proper CLI for desktop application

--- macroquad/src/web.rs
+++ macroquad/src/web.rs
@@ -4,11 +4,11 @@ extern "C" {
     fn get_rom_data() -> JsObject;
 }
 
-pub fn init() -> Box<dyn rustyboy_core::mbc::MemoryBankController> {
+pub fn init() -> Option<Box<dyn rustyboy_core::mbc::MemoryBankController>> {
     let js_obj = unsafe { get_rom_data() };
 
     let mut buf = Vec::new();
     js_obj.to_byte_buffer(&mut buf);
 
-    rustyboy_core::mbc::from_rom_data(&buf)
+    Some(rustyboy_core::mbc::from_rom_data(&buf))
 }
