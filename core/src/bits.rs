8d4b6f3cd6fde22bcb2d6473824b6bdd4029cbfa
bmoxb <maxoblack@yahoo.com>
2023-02-23 19:30:55 +0000

    add function for getting bit ranges, macro for bit get/set/toggle methods, and GPU types

--- core/src/bits.rs
+++ core/src/bits.rs
@@ -16,3 +16,43 @@ pub fn get_bit(value: u8, bit: u8) -> bool {
     let mask = 1 << bit;
     (value & mask) != 0
 }
+
+pub fn get_bits(value: u8, from_inclusive: u8, to_exclusive: u8) -> u8 {
+    debug_assert!(from_inclusive <= to_exclusive);
+    let mask = 2_u8.pow((to_exclusive - from_inclusive) as u32) - 1;
+    (value >> from_inclusive) & mask
+}
+
+macro_rules! bit_accessors {
+    ($bit:literal, $get:ident) => {
+        pub fn $get(&self) -> bool {
+            crate::bits::get_bit(self.0, $bit)
+        }
+    };
+
+    ($bit:literal, $get:ident, $set:ident) => {
+        bit_accessors!($bit, $get);
+
+        pub fn $set(&mut self, value: bool) {
+            self.0 = crate::bits::modify_bit(self.0, $bit, value);
+        }
+    };
+
+    ($bit:literal, $get:ident, $set:ident, $toggle:ident) => {
+        bit_accessors!($bit, $get, $set);
+
+        pub fn $toggle(&mut self) {
+            self.0 = crate::bits::toggle_bit(self.0, $bit);
+        }
+    };
+}
+pub(crate) use bit_accessors;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn get_bits() {
+        assert_eq!(super::get_bits(0b11100, 1, 4), 0b110);
+        assert_eq!(super::get_bits(1, 0, 0), 0);
+    }
+}
--- core/src/gpu/mod.rs
+++ core/src/gpu/mod.rs
@@ -1,9 +1,12 @@
+use crate::bits::{bit_accessors, get_bits};
+use crate::register_type;
+
 const VRAM_SIZE: usize = 0x2000;
 
 pub struct Gpu {
     pub vram: [u8; VRAM_SIZE],
-    pub lcd_control: u8,
-    pub lcd_status: u8,
+    pub lcd_control: LcdControlRegister,
+    pub lcd_status: LcdStatusRegister,
     pub viewport_y: u8,
     pub viewport_x: u8,
     pub lcd_y: u8,
@@ -19,8 +22,8 @@ impl Gpu {
     pub fn new() -> Self {
         Gpu {
             vram: [0; VRAM_SIZE],
-            lcd_control: 0x91,
-            lcd_status: 0x81,
+            lcd_control: LcdControlRegister(0x91),
+            lcd_status: LcdStatusRegister(0x81),
             viewport_y: 0,
             viewport_x: 0,
             lcd_y: 0x91,
@@ -32,4 +35,41 @@ impl Gpu {
             window_x: 0,
         }
     }
+}
+
+register_type!(LcdControlRegister);
+
+#[allow(dead_code)]
+impl LcdControlRegister {
+    bit_accessors!(0, bg_and_window_enable);
+    bit_accessors!(1, obj_enable);
+    bit_accessors!(2, obj_size);
+    bit_accessors!(3, bg_tile_map_area);
+    bit_accessors!(4, bg_and_window_tile_data_area);
+    bit_accessors!(5, window_enable);
+    bit_accessors!(6, window_tile_map_area);
+    bit_accessors!(7, lcd_enable);
+}
+
+register_type!(LcdStatusRegister);
+
+#[allow(dead_code)]
+impl LcdStatusRegister {
+    fn status(&self) -> LcdStatus {
+        match get_bits(self.0, 0, 2) {
+            0 => LcdStatus::HBlank,
+            1 => LcdStatus::VBlank,
+            2 => LcdStatus::SearchingOAM,
+            3 => LcdStatus::TransferringDataToController,
+            _ => unreachable!(),
+        }
+    }
+    bit_accessors!(2, ly_and_lyc_are_equal);
+}
+
+enum LcdStatus {
+    HBlank,
+    VBlank,
+    SearchingOAM,
+    TransferringDataToController,
 }
