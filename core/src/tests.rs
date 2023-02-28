82a0b7e0172c8ff681c85c4982681279e9090868
bmoxb <maxoblack@yahoo.com>
2023-02-28 11:30:02 +0000

    add instruction timing test

--- core/src/tests.rs
+++ core/src/tests.rs
@@ -10,7 +10,7 @@ macro_rules! test_rom {
 
             let rom = include_bytes!(concat!(
                 env!("CARGO_MANIFEST_DIR"),
-                "/../gb-test-roms/cpu_instrs/individual/",
+                "/../gb-test-roms/",
                 $file
             ));
             let mbc = mbc::from_rom_data(rom);
@@ -44,14 +44,22 @@ macro_rules! test_rom {
     };
 }
 
-test_rom!(blargg_rom_special, "01-special.gb");
-test_rom!(blargg_rom_interrupts, "02-interrupts.gb");
-test_rom!(blargg_rom_op_sp_hl, "03-op sp,hl.gb");
-test_rom!(blargg_rom_op_r_imm, "04-op r,imm.gb");
-test_rom!(blargg_rom_op_rp, "05-op rp.gb");
-test_rom!(blargg_rom_ld_r_r, "06-ld r,r.gb");
-test_rom!(blargg_rom_jr_jp_call_ret_rst, "07-jr,jp,call,ret,rst.gb");
-test_rom!(blargg_rom_misc, "08-misc instrs.gb");
-test_rom!(blargg_rom_op_r_r, "09-op r,r.gb");
-test_rom!(blargg_rom_bit_ops, "10-bit ops.gb");
-test_rom!(blargg_rom_op_a_hl, "11-op a,(hl).gb");
+test_rom!(cpu_instrs_special, "cpu_instrs/individual/01-special.gb");
+test_rom!(
+    cpu_instrs_interrupts,
+    "cpu_instrs/individual/02-interrupts.gb"
+);
+test_rom!(cpu_instrs_op_sp_hl, "cpu_instrs/individual/03-op sp,hl.gb");
+test_rom!(cpu_instrs_op_r_imm, "cpu_instrs/individual/04-op r,imm.gb");
+test_rom!(cpu_instrs_op_rp, "cpu_instrs/individual/05-op rp.gb");
+test_rom!(cpu_instrs_ld_r_r, "cpu_instrs/individual/06-ld r,r.gb");
+test_rom!(
+    cpu_instrs_jr_jp_call_ret_rst,
+    "cpu_instrs/individual/07-jr,jp,call,ret,rst.gb"
+);
+test_rom!(cpu_instrs_misc, "cpu_instrs/individual/08-misc instrs.gb");
+test_rom!(cpu_instrs_op_r_r, "cpu_instrs/individual/09-op r,r.gb");
+test_rom!(cpu_instrs_bit_ops, "cpu_instrs/individual/10-bit ops.gb");
+test_rom!(cpu_instrs_op_a_hl, "cpu_instrs/individual/11-op a,(hl).gb");
+
+test_rom!(instr_timing, "instr_timing/instr_timing.gb");
