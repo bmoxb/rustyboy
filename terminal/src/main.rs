0297aa5c89c0a997d390f157331ddf0cbdb43980
bmoxb <maxoblack@yahoo.com>
2023-05-08 18:46:20 +0100

    rendering using Unicode characters

--- terminal/src/main.rs
+++ terminal/src/main.rs
@@ -27,19 +27,14 @@ fn main() -> crossterm::Result<()> {
 
         stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-        for x in 0..SCREEN_WIDTH as u8 {
-            for y in 0..SCREEN_HEIGHT as u8 {
-                let colour = match gb.screen().get(x, y) {
-                    Colour::Black => style::Color::Black,
-                    Colour::DarkGrey => style::Color::DarkGreen,
-                    Colour::LightGrey => style::Color::Green,
-                    Colour::White => style::Color::White,
-                };
+        for term_x in 0..(SCREEN_WIDTH as u16 / 2) {
+            for term_y in 0..(SCREEN_HEIGHT as u16 / 2) {
+                let (col, chr) = terminal_colour_and_character(&gb, term_x, term_y);
 
                 stdout
-                    .queue(cursor::MoveTo(x as u16, y as u16))?
-                    .queue(style::SetBackgroundColor(colour))?
-                    .queue(style::Print(" "))?;
+                    .queue(cursor::MoveTo(term_x, term_y))?
+                    .queue(style::SetForegroundColor(col))?
+                    .queue(style::Print(chr))?;
             }
         }
 
@@ -47,6 +42,67 @@ fn main() -> crossterm::Result<()> {
 
         std::thread::sleep(std::time::Duration::from_secs_f32(0.1))
     }
+}
+
+fn terminal_colour_and_character(gb: &GameBoy, term_x: u16, term_y: u16) -> (style::Color, char) {
+    let screen = gb.screen();
+
+    let gb_x = term_x as u8 * 2;
+    let gb_y = term_y as u8 * 2;
+
+    let top_left = screen.get(gb_x, gb_y);
+    let top_right = screen.get(gb_x + 1, gb_y);
+    let bottom_left = screen.get(gb_x, gb_y + 1);
+    let bottom_right = screen.get(gb_x + 1, gb_y + 1);
+    let colours = [top_left, top_right, bottom_left, bottom_right];
+
+    let filtered_rgb_values = colours.iter().map(|c| match c {
+        Colour::Black => [15, 56, 15],
+        Colour::DarkGrey => [48u16, 98, 48],
+        Colour::LightGrey => [139, 172, 15],
+        Colour::White => [155, 188, 15],
+    });
+
+    let mut interpolated = filtered_rgb_values.fold([0, 0, 0], |mut col, y| {
+        for i in 0..3 {
+            col[i] += y[i];
+        }
+        col
+    });
+    for i in 0..3 {
+        interpolated[i] /= 3;
+    }
+    let col = style::Color::Rgb {
+        r: interpolated[0] as u8,
+        g: interpolated[1] as u8,
+        b: interpolated[2] as u8,
+    };
+
+    let mut blacks: [bool; 4] = [false; 4];
+    for (i, c) in colours.into_iter().enumerate() {
+        blacks[i] = matches!(c, Colour::Black);
+    }
+
+    let chr = match blacks {
+        // tl    tr    bl    br
+        [true, false, false, false] => '▘',
+        [false, true, false, false] => '▝',
+        [false, false, true, false] => '▖',
+        [false, false, false, true] => '▗',
+        [true, false, true, false] => '▌',
+        [false, true, false, true] => '▐',
+        [true, true, false, false] => '▀',
+        [false, false, true, true] => '▄',
+        [true, true, true, false] => '▛',
+        [true, true, false, true] => '▜',
+        [true, false, true, true] => '▙',
+        [false, true, true, true] => '▟',
+        [true, false, false, true] => '▚',
+        [false, true, true, false] => '▞',
+        _ => '█',
+    };
+
+    (col, chr)
 }
 
 #[derive(Parser)]
