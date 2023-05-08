b17606fde4416539af1d9f4dc63c394e52eceffa
bmoxb <maxoblack@yahoo.com>
2023-05-08 14:29:00 +0100

    add proper CLI interface

---
+++ desktop/src/emulator.rs
@@ -0,0 +1,158 @@
+use super::Args;
+
+use std::{fs::File, time::Instant};
+
+use pixels::{Pixels, SurfaceTexture};
+use rustyboy_core::{
+    joypad::Button,
+    mbc,
+    screen::{Colour, SCREEN_HEIGHT, SCREEN_WIDTH},
+    GameBoy,
+};
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+pub struct Emulator {
+    args: Args,
+    event_loop: Option<EventLoop<()>>,
+    window: Window,
+    pixels: Pixels,
+    gb: GameBoy,
+    last_instant: Instant,
+}
+
+impl Emulator {
+    pub fn new(args: Args) -> Self {
+        let event_loop = EventLoop::new();
+
+        let window = WindowBuilder::new()
+            .with_title("rustyboy")
+            .build(&event_loop)
+            .unwrap();
+
+        let pixels = {
+            let window_size = window.inner_size();
+            let surface_texture =
+                SurfaceTexture::new(window_size.width, window_size.height, &window);
+            Pixels::new(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32, surface_texture).unwrap()
+        };
+
+        let mbc = mbc::from_rom_file(&args.rom).unwrap();
+
+        let mut gb = GameBoy::new(mbc);
+
+        if let Some(path) = &args.gb_doctor_log {
+            let f = File::create(path).unwrap();
+            gb.enable_gb_doctor_logging(Box::new(f));
+        }
+
+        if let Some(_path) = &args.serial_log {
+            unimplemented!() // TODO
+        }
+
+        Emulator {
+            args,
+            event_loop: Some(event_loop),
+            window,
+            pixels,
+            gb,
+            last_instant: Instant::now(),
+        }
+    }
+
+    pub fn run(mut self) {
+        let e = self.event_loop.take().unwrap(); // the option is just allow us to move out of the struct so can safely unwrap here
+
+        e.run(move |event, _, control_flow| match event {
+            Event::MainEventsCleared => {
+                self.update();
+            }
+
+            Event::RedrawRequested(window_id) if window_id == self.window.id() => {
+                self.draw();
+            }
+
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == self.window.id() => {
+                self.handle_window_event(event, control_flow);
+            }
+
+            _ => {}
+        });
+    }
+
+    fn update(&mut self) {
+        let delta = (Instant::now() - self.last_instant).as_secs_f32();
+        self.last_instant = Instant::now();
+
+        self.gb.update(delta * self.args.speed);
+
+        self.window.request_redraw();
+    }
+
+    fn draw(&mut self) {
+        for (i, pixel) in self.pixels.frame_mut().chunks_exact_mut(4).enumerate() {
+            let x = (i % SCREEN_WIDTH) as u8;
+            let y = (i / SCREEN_WIDTH) as u8;
+
+            let rgba = match self.gb.screen().get(x, y) {
+                Colour::Black => [15, 56, 15, 255],
+                Colour::DarkGrey => [48, 98, 48, 255],
+                Colour::LightGrey => [139, 172, 15, 255],
+                Colour::White => [155, 188, 15, 255],
+            };
+
+            pixel.copy_from_slice(&rgba);
+        }
+
+        self.pixels.render().unwrap();
+    }
+
+    fn handle_window_event(&mut self, event: &WindowEvent, control_flow: &mut ControlFlow) {
+        match event {
+            WindowEvent::CloseRequested => {
+                *control_flow = ControlFlow::Exit;
+            }
+
+            WindowEvent::Resized(size) => {
+                self.pixels.resize_surface(size.width, size.height).unwrap();
+            }
+
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        virtual_keycode: Some(key),
+                        state,
+                        ..
+                    },
+                ..
+            } => {
+                let down = matches!(state, ElementState::Pressed);
+                self.handle_keyboard_input(key, down);
+            }
+
+            _ => {}
+        };
+    }
+
+    fn handle_keyboard_input(&mut self, key: &VirtualKeyCode, down: bool) {
+        let jp = self.gb.joypad();
+
+        match key {
+            VirtualKeyCode::X => jp.set_button(Button::A, down),
+            VirtualKeyCode::Z => jp.set_button(Button::B, down),
+            VirtualKeyCode::Return => jp.set_button(Button::Start, down),
+            VirtualKeyCode::RShift => jp.set_button(Button::Select, down),
+            VirtualKeyCode::Up => jp.set_button(Button::Up, down),
+            VirtualKeyCode::Down => jp.set_button(Button::Down, down),
+            VirtualKeyCode::Left => jp.set_button(Button::Left, down),
+            VirtualKeyCode::Right => jp.set_button(Button::Right, down),
+            _ => {}
+        };
+    }
+}
--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,144 +1,28 @@
-use std::time::Instant;
+mod emulator;
 
-use pixels::{Pixels, SurfaceTexture};
-use rustyboy_core::{
-    joypad::Button,
-    mbc,
-    screen::{Colour, SCREEN_HEIGHT, SCREEN_WIDTH},
-    GameBoy,
-};
-use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder},
-};
+use std::path::PathBuf;
+
+use clap::Parser;
 
 fn main() {
     env_logger::init();
-
-    let event_loop = EventLoop::new();
-
-    let window = WindowBuilder::new()
-        .with_title("rustyboy")
-        .build(&event_loop)
-        .unwrap();
-
-    let pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32, surface_texture).unwrap()
-    };
-
-    let args: Vec<String> = std::env::args().collect();
-    let path = &args[1];
-    let mbc = mbc::from_rom_file(path).unwrap();
-    let gb = GameBoy::new(mbc);
-
-    let emu = Emulator {
-        window,
-        pixels,
-        gb,
-        last_instant: Instant::now(),
-    };
-    emu.run(event_loop)
+    let args = Args::parse();
+    let emu = emulator::Emulator::new(args);
+    emu.run()
 }
 
-struct Emulator {
-    window: Window,
-    pixels: Pixels,
-    gb: GameBoy,
-    last_instant: Instant,
-}
-
-impl Emulator {
-    fn run(mut self, event_loop: EventLoop<()>) {
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::MainEventsCleared => {
-                self.update();
-            }
-
-            Event::RedrawRequested(window_id) if window_id == self.window.id() => {
-                self.draw();
-            }
-
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == self.window.id() => {
-                self.handle_window_event(event, control_flow);
-            }
-
-            _ => {}
-        });
-    }
-
-    fn update(&mut self) {
-        let delta = (Instant::now() - self.last_instant).as_secs_f32();
-        self.last_instant = Instant::now();
-
-        self.gb.update(delta);
-
-        self.window.request_redraw();
-    }
-
-    fn draw(&mut self) {
-        for (i, pixel) in self.pixels.frame_mut().chunks_exact_mut(4).enumerate() {
-            let x = (i % SCREEN_WIDTH) as u8;
-            let y = (i / SCREEN_WIDTH) as u8;
-
-            let rgba = match self.gb.screen().get(x, y) {
-                Colour::Black => [15, 56, 15, 255],
-                Colour::DarkGrey => [48, 98, 48, 255],
-                Colour::LightGrey => [139, 172, 15, 255],
-                Colour::White => [155, 188, 15, 255],
-            };
-
-            pixel.copy_from_slice(&rgba);
-        }
-
-        self.pixels.render().unwrap();
-    }
-
-    fn handle_window_event(&mut self, event: &WindowEvent, control_flow: &mut ControlFlow) {
-        match event {
-            WindowEvent::CloseRequested => {
-                *control_flow = ControlFlow::Exit;
-            }
-
-            WindowEvent::Resized(size) => {
-                self.pixels.resize_surface(size.width, size.height).unwrap();
-            }
-
-            WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        virtual_keycode: Some(key),
-                        state,
-                        ..
-                    },
-                ..
-            } => {
-                let down = matches!(state, ElementState::Pressed);
-                self.handle_keyboard_input(key, down);
-            }
-
-            _ => {}
-        };
-    }
-
-    fn handle_keyboard_input(&mut self, key: &VirtualKeyCode, down: bool) {
-        let jp = self.gb.joypad();
-
-        match key {
-            VirtualKeyCode::X => jp.set_button(Button::A, down),
-            VirtualKeyCode::Z => jp.set_button(Button::B, down),
-            VirtualKeyCode::Return => jp.set_button(Button::Start, down),
-            VirtualKeyCode::RShift => jp.set_button(Button::Select, down),
-            VirtualKeyCode::Up => jp.set_button(Button::Up, down),
-            VirtualKeyCode::Down => jp.set_button(Button::Down, down),
-            VirtualKeyCode::Left => jp.set_button(Button::Left, down),
-            VirtualKeyCode::Right => jp.set_button(Button::Right, down),
-            _ => {}
-        };
-    }
+#[derive(Parser)]
+pub struct Args {
+    /// Path to a Game Boy ROM file to execute
+    rom: PathBuf,
+    // Speed multiplier at which to run the emulator
+    #[arg(short, long, default_value = "1.0")]
+    speed: f32,
+    /// Write the text written to serial out by debugging/test ROMs to a given file
+    #[arg(long)]
+    serial_log: Option<PathBuf>,
+    /// Write Game Boy Doctor log lines to a given file - this option should be used with a very low speed setting
+    /// (e.g., 0.01) so as to avoid crashes
+    #[arg(long)]
+    gb_doctor_log: Option<PathBuf>,
 }
