63aca1693eb5ef49829de17a7046ae24443d7493
bmoxb <maxoblack@yahoo.com>
2023-05-09 19:25:56 +0100

    organise terminal code

--- terminal/src/main.rs
+++ terminal/src/main.rs
@@ -1,7 +1,7 @@
 use rustyboy_core::{
     joypad::Button,
     mbc,
-    screen::{Colour, Screen, SCREEN_HEIGHT, SCREEN_WIDTH},
+    screen::{Colour, SCREEN_HEIGHT, SCREEN_WIDTH},
     GameBoy,
 };
 
@@ -24,19 +24,26 @@ use std::{
 fn main() -> crossterm::Result<()> {
     let args = Args::parse();
 
-    let mut stdout = std::io::stdout();
+    let mbc = mbc::from_rom_file(&args.rom).unwrap();
+    let gb = GameBoy::new(mbc);
 
     terminal::enable_raw_mode()?;
-    stdout
+    std::io::stdout()
         .execute(PushKeyboardEnhancementFlags(
             KeyboardEnhancementFlags::REPORT_EVENT_TYPES,
         ))?
         .execute(terminal::Clear(terminal::ClearType::All))?;
 
-    let result = run(&mut stdout, args);
+    let mut emu = Emulator {
+        args,
+        stdout: std::io::stdout(),
+        gb,
+        continue_execution: true,
+    };
+    let result = emu.run();
 
     terminal::disable_raw_mode()?;
-    stdout
+    std::io::stdout()
         .execute(PopKeyboardEnhancementFlags)?
         .execute(style::SetBackgroundColor(style::Color::Black))?
         .execute(style::SetForegroundColor(style::Color::White))?
@@ -45,23 +52,47 @@ fn main() -> crossterm::Result<()> {
     result
 }
 
-fn run(stdout: &mut Stdout, args: Args) -> crossterm::Result<()> {
-    let mbc = mbc::from_rom_file(&args.rom).unwrap();
-    let mut gb = GameBoy::new(mbc);
+#[derive(Parser)]
+pub struct Args {
+    /// Path to a Game Boy ROM file to execute
+    rom: PathBuf,
+    /// Disable full RGB colours and use a more limited palette
+    #[arg(long, default_value = "false")]
+    no_rgb: bool,
+    /// Disable Unicode characters
+    #[arg(long, default_value = "false")]
+    no_unicode: bool,
+}
 
-    let mut last_instant = Instant::now();
+struct Emulator {
+    args: Args,
+    stdout: Stdout,
+    gb: GameBoy,
+    continue_execution: bool,
+}
 
-    loop {
-        let delta = (Instant::now() - last_instant).as_secs_f32();
-        last_instant = Instant::now();
+impl Emulator {
+    fn run(&mut self) -> crossterm::Result<()> {
+        let mut last_instant = Instant::now();
 
-        gb.update(delta);
+        while self.continue_execution {
+            let delta = (Instant::now() - last_instant).as_secs_f32();
+            last_instant = Instant::now();
 
+            self.gb.update(delta);
+            self.draw()?;
+            self.handle_events()?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self) -> crossterm::Result<()> {
         for term_x in 0..SCREEN_WIDTH as u16 {
             for term_y in 0..(SCREEN_HEIGHT / 2) as u16 {
-                let (chr, fg, bg) = choose_character_and_colour(gb.screen(), term_x, term_y, &args);
+                let (chr, fg, bg) = self.choose_character_and_colour(term_x, term_y);
 
-                stdout
+                self.stdout
                     .queue(cursor::MoveTo(term_x, term_y))?
                     .queue(style::SetForegroundColor(fg))?
                     .queue(style::SetBackgroundColor(bg))?
@@ -69,67 +100,76 @@ fn run(stdout: &mut Stdout, args: Args) -> crossterm::Result<()> {
             }
         }
 
-        stdout.flush().unwrap();
+        self.stdout.flush()
+    }
 
+    fn choose_character_and_colour(
+        &self,
+        term_x: u16,
+        term_y: u16,
+    ) -> (char, style::Color, style::Color) {
+        let up = self.gb.screen().get(term_x as u8, term_y as u8 * 2);
+        let down = self.gb.screen().get(term_x as u8, term_y as u8 * 2 + 1);
+
+        let chr = if self.args.no_unicode {
+            colours_to_ascii(up, down)
+        } else {
+            colours_to_unicode(up, down)
+        };
+
+        let (up_col, down_col) = if self.args.no_rgb {
+            (gb_colour_to_term_colour(up), gb_colour_to_term_colour(down))
+        } else {
+            (gb_colour_to_rgb_colour(up), gb_colour_to_rgb_colour(down))
+        };
+
+        let (fg, bg) = if up > down {
+            (down_col, up_col)
+        } else {
+            (up_col, down_col)
+        };
+
+        (chr, fg, bg)
+    }
+
+    fn handle_events(&mut self) -> crossterm::Result<()> {
         if event::poll(Duration::from_millis(5))? {
             match event::read()? {
                 Event::Key(KeyEvent { code, kind, .. }) => {
                     let down = !matches!(kind, KeyEventKind::Release);
-                    let jp = gb.joypad();
-                    match code {
-                        KeyCode::Char('x') => jp.set_button(Button::A, down),
-                        KeyCode::Char('z') => jp.set_button(Button::B, down),
-                        KeyCode::Enter => jp.set_button(Button::Start, down),
-                        KeyCode::Backspace => jp.set_button(Button::Select, down),
-                        KeyCode::Up => jp.set_button(Button::Up, down),
-                        KeyCode::Down => jp.set_button(Button::Down, down),
-                        KeyCode::Left => jp.set_button(Button::Left, down),
-                        KeyCode::Right => jp.set_button(Button::Right, down),
-                        KeyCode::Esc => {
-                            return Ok(());
-                        }
-                        _ => {}
-                    }
+                    self.handle_key_event(code, down);
                 }
 
                 Event::Resize(_, _) => {
-                    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+                    self.stdout
+                        .execute(terminal::Clear(terminal::ClearType::All))?;
                 }
 
                 _ => {}
             }
         }
+
+        Ok(())
     }
-}
 
-fn choose_character_and_colour(
-    screen: &Screen,
-    term_x: u16,
-    term_y: u16,
-    args: &Args,
-) -> (char, style::Color, style::Color) {
-    let up = screen.get(term_x as u8, term_y as u8 * 2);
-    let down = screen.get(term_x as u8, term_y as u8 * 2 + 1);
+    fn handle_key_event(&mut self, code: KeyCode, down: bool) {
+        let jp = self.gb.joypad();
 
-    let chr = if args.no_unicode {
-        colours_to_ascii(up, down)
-    } else {
-        colours_to_unicode(up, down)
-    };
-
-    let (up_col, down_col) = if args.no_rgb {
-        (gb_colour_to_term_colour(up), gb_colour_to_term_colour(down))
-    } else {
-        (gb_colour_to_rgb_colour(up), gb_colour_to_rgb_colour(down))
-    };
-
-    let (fg, bg) = if up > down {
-        (down_col, up_col)
-    } else {
-        (up_col, down_col)
-    };
-
-    (chr, fg, bg)
+        match code {
+            KeyCode::Char('x') => jp.set_button(Button::A, down),
+            KeyCode::Char('z') => jp.set_button(Button::B, down),
+            KeyCode::Enter => jp.set_button(Button::Start, down),
+            KeyCode::Backspace => jp.set_button(Button::Select, down),
+            KeyCode::Up => jp.set_button(Button::Up, down),
+            KeyCode::Down => jp.set_button(Button::Down, down),
+            KeyCode::Left => jp.set_button(Button::Left, down),
+            KeyCode::Right => jp.set_button(Button::Right, down),
+            KeyCode::Esc => {
+                self.continue_execution = false;
+            }
+            _ => {}
+        }
+    }
 }
 
 fn colours_to_ascii(up: Colour, down: Colour) -> char {
@@ -184,16 +224,4 @@ fn gb_colour_to_rgb_colour(c: Colour) -> style::Color {
             b: 15,
         },
     }
-}
-
-#[derive(Parser)]
-pub struct Args {
-    /// Path to a Game Boy ROM file to execute
-    rom: PathBuf,
-    /// Disable full RGB colours and use a more limited palette
-    #[arg(long, default_value = "false")]
-    no_rgb: bool,
-    /// Disable Unicode characters
-    #[arg(long, default_value = "false")]
-    no_unicode: bool,
 }
