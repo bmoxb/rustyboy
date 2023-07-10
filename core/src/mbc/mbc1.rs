c8f68ea3a41177db7a64b02b3d4c0b487aa5afce
bmoxb <maxoblack@yahoo.com>
2023-07-10 23:58:02 +0200

    add RAM to MBC1

--- core/src/mbc/mbc1.rs
+++ core/src/mbc/mbc1.rs
@@ -9,14 +9,17 @@ pub struct MBC1 {
     ram_enable: bool,
     /// The current ROM bank (this variable merges the two separate 5-bit and 2-bit registers).
     rom_bank: u8,
+    /// RAM stored on the MBC (if any).
+    ram: Option<Vec<u8>>,
 }
 
 impl MBC1 {
-    pub fn new(cart: Cartridge, _has_ram: bool, _has_battery: bool) -> Self {
+    pub fn new(cart: Cartridge, has_ram: bool, _has_battery: bool) -> Self {
         MBC1 {
             cart,
             ram_enable: false,
             rom_bank: 1,
+            ram: has_ram.then(|| vec![0; 0x2000]), // TODO: handle larger RAM size
         }
     }
 }
@@ -28,7 +31,15 @@ impl super::MemoryBankController for MBC1 {
             0x4000..=0x7FFF => self
                 .cart
                 .read8((self.rom_bank as usize * 0x4000) + addr as usize - 0x4000),
-            0xA000..=0xBFFF => 0,
+            0xA000..=0xBFFF => {
+                if let Some(ram) = &self.ram {
+                    if self.ram_enable {
+                        // TODO: RAM banking
+                        return ram[(addr - 0xA000) as usize];
+                    }
+                }
+                0xFF
+            }
             _ => 0,
         }
     }
@@ -42,7 +53,14 @@ impl super::MemoryBankController for MBC1 {
             }
             0x4000..=0x5FFF => {}
             0x6000..=0x7FFF => {}
-            0xA000..=0xBFFF => {}
+            0xA000..=0xBFFF => {
+                if let Some(ram) = &mut self.ram {
+                    if self.ram_enable {
+                        // TODO: RAM banking
+                        ram[(addr - 0xA000) as usize] = value;
+                    }
+                }
+            }
             _ => {}
         }
     }
@@ -51,5 +69,7 @@ impl super::MemoryBankController for MBC1 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // TODO
+
+    //#[test]
+    //fn read_
 }
